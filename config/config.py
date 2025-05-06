56b9968732e46e1702bf3c5a92718061c2819681
Mehmet67676767 <kurt67143@gmail.com>
2025-05-06 21:59:14 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -43,7 +43,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/zerosugar6464/Musicbot")
 
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN",  None)
 
