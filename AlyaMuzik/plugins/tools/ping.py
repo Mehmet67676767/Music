1853a9c0806704f0101f52cf53093d6978a5aabe
Mehmet67676767 <kurt67143@gmail.com>
2025-05-06 22:18:24 +0300

    Add files via upload

---
+++ AlyaMuzik/plugins/tools/ping.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2025 by Alya_Help @ Github, < https://github.com/TheTeamAlya >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alya © Yukki.
+
+"""
+TheTeamAlya is a project of Telegram bots with variety of purposes.
+Copyright (c) 2021 ~ Present Team Alya <https://github.com/TheTeamAlya>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
+from strings import get_command
+from AlyaMuzik import app
+from AlyaMuzik.core.call import Alya
+from AlyaMuzik.utils import bot_sys_stats
+from AlyaMuzik.utils.decorators.language import language
+
+### Commands
+PING_COMMAND = get_command("PING_COMMAND")
+
+
+@app.on_message(filters.command(PING_COMMAND) & filters.group & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"],
+    )
+    start = datetime.now()
+    pytgping = await Alya.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping)
+    )
