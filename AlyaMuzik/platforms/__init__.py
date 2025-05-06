4282cc3dd5835389668962d41c8da322424741ae
Mehmet67676767 <kurt67143@gmail.com>
2025-05-06 21:06:44 +0300

    Add files via upload

---
+++ AlyaMuzik/platforms/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2025 by Alya_Help @ Github, < https://github.com/TheTeamAlya >
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alya © Yukki.
+
+"""
+TheTeamAlya is a project of Telegram bots with variety of purposes.
+Copyright (c) 2021 ~ Present Team Alya <https://github.com/TheTeamAlya>
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
