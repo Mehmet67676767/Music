2ddfac451c6ab02a10cddbb476ef5308ecd9334c
Mehmet67676767 <kurt67143@gmail.com>
2025-05-06 21:36:07 +0300

    Add files via upload

---
+++ strings/helpers.py
@@ -0,0 +1,108 @@
+HELP_1 = """<b><u>ᴀᴅᴍɪɴ ᴋᴏᴍᴜᴛʟᴀʀı :</b></u>
+╔─━━━━░...𝚂𝙸𝙻𝙰 𝙼𝚄𝚉𝙸𝙺....░━━━─╗
+╰☞ /durdur /pause - Akışı duraklatır.
+╰☞ /devam /resume - Akışı devam ettirir.
+╰☞ /son - /end - Akışı Sonlandırır.
+╰☞ /atla /skip - Diğer parça ya atlar.
+╰☞ /ileri - Oynatılan parçayı ileri alır.
+╰☞ /gerial - Oynatılan parçayı geri alır.
+╰☞ /karistir /suffle- Sıraya alınan Parçaları karışık oynatır.
+╰☞ /tekrarla /loop - oynatılan parçayı istediğiniz kadar tekrar eder.
+╰☞ /reload - Admin Önbelleğini yeniler.
+/reboot - Botu yeniden başlatır.
+────┈┈┈┄┄╌╌╌╌┄┄────┈┈┈┄┄╌╌╌╌┈┈┈────
+"""
+
+
+HELP_2 = """<b><u>command:</b></u>
+
+╔─━━━░...𝚂𝙸𝙻𝙰 𝙼𝚄𝚉𝙸𝙺....░━━━─╗
+
+
+▪️ /voynat - Video Oynatır.
+/oynat /voynat /play - /vplay 
+
+♨️🔸 /oynat komutu aynı zamanda canlı yayında destekler.(örnek: /oynat kralfm canlı)
+/playforce ᴏʀ /vplayforce : Sesli sohbette çalınan parçayı durdurur ve sırayı bozmadan temizlemeden aranan parçayı anında çalmaya başlar.
+
+örnek /oynat taladro dem
+
+•─────────────────────────────────•
+"""
+
+HELP_3 = """<b><u>komut:</b></u>
+
+╔─━━━░...𝚂𝙸𝙻𝙰 𝙼𝚄𝚉𝙸𝙺....░━━━─╗
+
+/indir-  - /indir [sᴏɴɢ ɴᴀᴍᴇ/ʏᴛ ᴜʀʟ] : ɪsᴛᴇᴅɪɢ̆ɪɴɪᴢ şᴀʀᴋıʏı ᴍᴘ𝟹 ᴏʟᴀʀᴀᴋ ɪɴᴅɪʀɪʀ.
+
+/ileri [ᴅᴜʀᴀᴛɪᴏɴ ɪɴ sᴇᴄᴏɴᴅs] : şᴀʀᴋıʏı ʙᴇʟɪʀʟᴇᴅɪɢ̆ɪɴɪᴢ sᴜ̈ʀᴇ ᴋᴀᴅᴀʀ ɪʟᴇʀɪ ᴀʟıʀ 
+
+/kariştir - /shuffle : Sıradaki parçaları karışık oynatır.
+
+/tekrar - /loop [1, 2, 3, ...] : ɢɪʀᴅɪɢ̆ɪɴɪᴢ ʀᴀᴋᴀᴍ ᴋᴀᴅᴀʀ ᴘᴀʀᴄ̧ᴀ ᴛᴇʀᴋᴀʀʟᴀɴıʀ
+
+/speed or /playback : ɢʀᴜᴘᴛᴀ sᴇsʟɪᴅᴇ ᴄ̧ᴀʟᴀɴ şᴀʀᴋıʏı ʜıᴢʟᴀɴᴅıʀıʀ.
+
+╰──────────────╮•╭─────────────╯
+"""
+HELP_4 = """<b><u> eğlenceli komutlar:</b></u>
+
+╔─━━━░...𝚂𝙸𝙻𝙰 𝙼𝚄𝚉𝙸𝙺....░━━━─╗
+
+❤️‍🔥  /eros | eros  –  Eros oku atar.
+
+📝  /burc  –  Burçlarınızı yorumlarım.
+
+✨ /mani  –  Mani söylerim.
+
+👋  /slap  –  Birini tokatlayın.
+
+🎲  /zar  –  Rastgele bir zar atın.
+
+🎯  /dart  –  Dart atar.
+
+🎰  /slot  –  Şans slot'u çevirir.
+
+⚽️  /futbol  –  Kaleye top atar.
+
+🎳  /bowling  –  Bowling atar.
+
+🪙  /para  –  Rastgele bir para atın.
+
+👋  /slap  –  Birini tokatlayın.
+
+🤣  /saka  –  Rastgele bir şaka gönderin.
+
+📜  /soz  –  Rastgele bir söz gönderin. 
+
+📢  /tts  –  Bir metni sese çevirir.
+◇──────────────────────────◇
+"""
+
+
+HELP_7 = """<b><u>etiket komutlar :</b></u>
+╔─━━━░...𝚂𝙸𝙻𝙰 𝙼𝚄𝚉𝙸𝙺....░━━━─╗
+
+
+🏷️ /tag - Tek tek etiketler.
+
+🏷️ /utag - Çoklu etiketler.
+
+🏷️ /etag - Emoji ile etiketler.
+
+🏷️ /igtag - iyi geceler mesajları ile etiketler.
+
+🏷️ /guntag - günaydın mesajları ile etiketler.
+
+🏷️ /btag - Bayrak ile etiketler.
+
+🏷️ /sorutag - Sorularla etiketler.
+
+🏷️ /ktag - Karakter ile etiketler.
+
+🏷️ /stag - Sözlerle etiketler.
+
+🏷️ /stop - Etiket işlemini bitirir.
+≪━─━─━──━─━─━─━─━─━──━─━─━─━━─━≫
+"""
